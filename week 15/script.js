56515b536e65c2901fbe174c9f1d75bdee1403d4
joshemma <61088326+joshemma@users.noreply.github.com>
2020-05-16 09:54:06 -0400

    update javascript

--- week 15/script.js
+++ week 15/script.js
@@ -1,40 +1,36 @@
 window.onload = function () {
 
-var background = document.getElementByClass ("Page")
-var button = document.getElementByClass("button")
+	var images = [
+	"img/IMG_2867.jpeg",
+	"img/IMG_2920.jpeg",
+	"img/IMG_2869.jpeg",
+	"img/IMG_9982.jpeg",
+	"img/IMG_2933.jpeg",
+	"img/IMG_5692.jpeg",
+	"img/IMG_2924.jpeg",
+	"img/IMG_5924.jpeg",
+	"img/IMG_6317.jpeg",
+	"img/IMG_2495.jpeg",
+	"img/IMG_1803.jpeg",
+	"img/IMG_1800.jpeg"
+	];
 
-var Page = [
-
-{
-	title: 'midnight'
-	imageFile: 'midnight.jpg'
-},
-{
-	title: 'sunset'
-	imageFile: 'sunset.jpg'
-},
-{
-	title: 'mountain'
-	imageFile: 'mountain.jpg'
-},
-
-];
+var image = $('.grid-item');
 
 function select_random(x){
-	y=x [Math.Floor(Math.random()*x.Length)];
+	y=x[Math.Floor(Math.random()*x.length)];
 	return y;
-}
-
-function generate(){
-	var background = selected_random(Page);
-
-	background.innerHTML = selected_one;
-}
-
-document.getElementByClass('button').onclick = function(){
-	console.log('click');
-	generate();
 };
-generate();
+
+      function display_image() {
+       var selected_image = select_random(images);
+        $('.grid-item').attr("src", selected_image);
+      };
+      $(document).ready(function(){
+        display_image();
+        $('button').click(function(){
+          display_image();
+        });
+    });
 
 }
